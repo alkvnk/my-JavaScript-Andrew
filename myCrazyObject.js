a0734c96795e91d1b53d61eb79515eb0281a8530
Alexey <alkvnk@gmail.com>
2019-09-02 22:12:22 +0300

    functionReturn

--- myCrazyObject.js
+++ myCrazyObject.js
@@ -3,3 +3,13 @@ var myCrazyObject = {
     "some array": [7, 9, {purpose: "confusion", number: 123}, 3.3],
     "random animal": "banana shark"
 };
+function monitorCount(rows, columns) {
+    return rows * columns;
+  };
+  
+  function costOfMonitors(rows, columns) {
+    return monitorCount(rows, columns) * 200;
+  };
+  
+  const totalCost = costOfMonitors(5, 4);
+  console.log(totalCost);
